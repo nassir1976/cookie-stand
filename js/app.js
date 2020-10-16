e72563bbac6f6f2c995d96456e23df43903fda19
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-10-16 10:39:14 -0700

    modification

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@
 var operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var seattle= {
- minCustumerPerHr : 23,
- maxCustumerPerHr: 65,
- avgCookesPerSale: 6.3,
-getRandomCustomerPerHr:function(){
-  this.custemerPerHr = `${RandomCustomerPerHr(23,65)}`;
-  console.log(getRandomCustemerPerHr);
-}
- 
+  minCustomerPerHr : 23,
+  maxCustomerPerHr: 65,
+  avgCookiesSalePerCustomer: 6.3,
+  randomCustomerPerHr : [],
+ getcustomerPerHr: function(){
+   this.customerPerHr = `${randomCustomerPerHr(23, 65)}`;
+   console.log(`${this.customerPerHr}`);
+
+ }
+  
 };
 
-
-function randomCustemerPerHr(min,max) {
+function randomCustomerPerHr(min, max) {
 return Math.floor(Math.random() * (max - min + 1) + min);
 }
- seattle.getRandomCustemerPerHr();
+ seattle.getcustomerPerHr();
 // function getRandomIntInclusive(min, max) {
 //   min = Math.ceil(min);
 //   max = Math.floor(max);
   // return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
 // }
-
