4921facd36c2d28701f52262b6d640c04a72cb7e
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-10-16 01:23:40 -0700

    modification

--- js/app.js
+++ js/app.js
@@ -2,6 +2,24 @@
 var operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var seattle= {
-
+ minCustumerPerHr : 23,
+ maxCustumerPerHr: 65,
+ avgCookesPerSale: 6.3,
+getRandomCustomerPerHr:function(){
+  this.custemerPerHr = `${RandomCustomerPerHr(23,65)}`;
+  console.log(getRandomCustemerPerHr);
 }
+ 
+};
+
+
+function randomCustemerPerHr(min,max) {
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+ seattle.getRandomCustemerPerHr();
+// function getRandomIntInclusive(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+  // return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+// }
 
